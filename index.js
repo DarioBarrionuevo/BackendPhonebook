34b886a90127bd2a2cd3b315a121ae9fa4ae3bc5
Dario Barrionuevo <dariobafran@gmail.com>
2023-09-13 12:38:44 +0200

    Added PUT method

--- index.js
+++ index.js
@@ -121,6 +121,22 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+// UPDATE ONE PERSON
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 // -------------
 
 app.use(unknownEndpoint);
